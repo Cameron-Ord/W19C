914fd03629c5eaa3014c68b8b9f7d75bed57bbee
NuckenMcFuggets <cameron.ian.ronald.ord@Outlook.com>
2023-05-30 21:02:32 -0600

    initial commit

---
+++ input_handler.py
@@ -0,0 +1,21 @@
+
+def attack_input():
+    
+    print("ATTACKS:")
+    print("1. Punch")
+    print("2. Kick")
+
+    attack = input("choose an attack")
+
+    attack = int(attack)
+
+    if(attack == 1):
+
+        return attack
+    
+    elif(attack == 2):
+
+        return attack
+
+
+attack_input()
